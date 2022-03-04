3c657ffedad66050a12a129dc6c568373de509fd
boyacioglu <hansbuda16@gmail.com>
2022-03-04 14:16:09 +0100

    Last statement at the end of the project

--- src/Store.java
+++ src/Store.java
@@ -4,5 +4,6 @@ public class Store {
     public static void main(String[] args) {
         System.out.println("Forth work");
         System.out.println("More work at the end of the project");
+        System.out.println("Last statement");
     }
 }
