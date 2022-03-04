ccec39c2c363687880faa3d88c22f79f9c27c043
boyacioglu <hansbuda16@gmail.com>
2022-03-04 14:15:11 +0100

    one more work at the end of the project

--- src/Store.java
+++ src/Store.java
@@ -3,5 +3,6 @@ public class Store {
 
     public static void main(String[] args) {
         System.out.println("Forth work");
+        System.out.println("More work at the end of the project");
     }
 }
