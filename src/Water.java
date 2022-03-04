a7d71da0c30a5785e4db1652169116b43d1ecae8
boyacioglu <hansbuda16@gmail.com>
2022-03-04 09:27:22 +0100

    First Day at Work

---
+++ src/Water.java
@@ -0,0 +1,6 @@
+public class Water {
+    public static void main(String[] args) {
+        System.out.println("First day at work");
+        System.out.println("did UI  Automation, did verification of DB and UI");
+    }
+}
